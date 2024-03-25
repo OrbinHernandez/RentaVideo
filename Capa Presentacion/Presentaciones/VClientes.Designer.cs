9d920def336830a7bed6c7d91150fba756f316ef
Jesús Eduardo <9107140aa8@gmail.com>
2024-03-24 20:47:03 -0600

    implementacion en capas de las tablas Peliculas/Clientes

---
+++ Capa Presentacion/Presentaciones/VClientes.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Capa_Presentacion
+{
+    partial class VClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.BtnEditar = new System.Windows.Forms.Button();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.DgvDatos = new System.Windows.Forms.DataGridView();
+            this.Seleccion = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.ChkActivo = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtApellidos = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.LblId = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDatos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clientes";
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.BackColor = System.Drawing.Color.IndianRed;
+            this.BtnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEliminar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnEliminar.Location = new System.Drawing.Point(577, 64);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(111, 31);
+            this.BtnEliminar.TabIndex = 34;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = false;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // BtnEditar
+            // 
+            this.BtnEditar.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.BtnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEditar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnEditar.Location = new System.Drawing.Point(577, 30);
+            this.BtnEditar.Name = "BtnEditar";
+            this.BtnEditar.Size = new System.Drawing.Size(111, 31);
+            this.BtnEditar.TabIndex = 33;
+            this.BtnEditar.Text = "Editar";
+            this.BtnEditar.UseVisualStyleBackColor = false;
+            this.BtnEditar.Click += new System.EventHandler(this.BtnEditar_Click);
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.BtnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGuardar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnGuardar.Location = new System.Drawing.Point(46, 119);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(613, 31);
+            this.BtnGuardar.TabIndex = 32;
+            this.BtnGuardar.Text = "Guardar Cambios";
+            this.BtnGuardar.UseVisualStyleBackColor = false;
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // DgvDatos
+            // 
+            this.DgvDatos.AllowUserToAddRows = false;
+            this.DgvDatos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DgvDatos.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.DgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Seleccion});
+            this.DgvDatos.Location = new System.Drawing.Point(22, 175);
+            this.DgvDatos.Name = "DgvDatos";
+            this.DgvDatos.Size = new System.Drawing.Size(657, 250);
+            this.DgvDatos.TabIndex = 31;
+            // 
+            // Seleccion
+            // 
+            this.Seleccion.HeaderText = "";
+            this.Seleccion.Name = "Seleccion";
+            // 
+            // ChkActivo
+            // 
+            this.ChkActivo.AutoSize = true;
+            this.ChkActivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChkActivo.Location = new System.Drawing.Point(370, 39);
+            this.ChkActivo.Name = "ChkActivo";
+            this.ChkActivo.Size = new System.Drawing.Size(67, 22);
+            this.ChkActivo.TabIndex = 30;
+            this.ChkActivo.Text = "Activo";
+            this.ChkActivo.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(309, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 18);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Estado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(54, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 18);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Apellido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Nombre";
+            // 
+            // TxtApellidos
+            // 
+            this.TxtApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtApellidos.Location = new System.Drawing.Point(120, 67);
+            this.TxtApellidos.Multiline = true;
+            this.TxtApellidos.Name = "TxtApellidos";
+            this.TxtApellidos.Size = new System.Drawing.Size(169, 29);
+            this.TxtApellidos.TabIndex = 21;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNombre.Location = new System.Drawing.Point(120, 32);
+            this.TxtNombre.Multiline = true;
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(169, 29);
+            this.TxtNombre.TabIndex = 20;
+            // 
+            // LblId
+            // 
+            this.LblId.AutoSize = true;
+            this.LblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblId.Location = new System.Drawing.Point(116, 9);
+            this.LblId.Name = "LblId";
+            this.LblId.Size = new System.Drawing.Size(19, 20);
+            this.LblId.TabIndex = 35;
+            this.LblId.Text = "0";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // VClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 454);
+            this.Controls.Add(this.LblId);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.BtnEditar);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.DgvDatos);
+            this.Controls.Add(this.ChkActivo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtApellidos);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "VClientes";
+            this.Text = "Clientes";
+            this.Load += new System.EventHandler(this.VClientes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDatos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Button BtnEditar;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.DataGridView DgvDatos;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Seleccion;
+        private System.Windows.Forms.CheckBox ChkActivo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtApellidos;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.Label LblId;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Capa Presentacion/Presentaciones/VPeliculas.cs
@@ -0,0 +1,143 @@
+﻿using Capa_Datos;
+using Capa_Datos.Modelos;
+using Capa_Negocios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Presentacion
+{
+    public partial class VPeliculas : Form
+    {
+        NPeliculas nPeliculas;
+        public VPeliculas()
+        {
+            InitializeComponent();
+            nPeliculas = new NPeliculas();
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            guardar();
+        }
+        private void Limpiar()
+        {
+            LblId.Text = "0";
+            TxtAutores.Text = "";
+            TxtExistencia.Text = "";
+            TxtNombre.Text = "";
+            TxtPrecioRenta.Text = "";
+            TxtGenero.Text = "";
+            ChkActivo.Checked = false;
+            errorProvider1.Clear();
+        }
+        void guardar()
+        {
+            string PeliculaId = LblId.Text;
+            string Nombre = TxtNombre.Text;
+            string Existencia = TxtExistencia.Text;
+            string Autores = TxtAutores.Text;
+            string Genero = TxtGenero.Text;
+            string PrecioRenta = TxtPrecioRenta.Text;
+
+            if (string.IsNullOrEmpty(Nombre) || string.IsNullOrWhiteSpace(Nombre))
+            {
+                errorProvider1.SetError(TxtNombre, "Debe colocar la Nombre");
+                return;
+            }
+
+            var pelicula = new Peliculas();
+            if (int.Parse(PeliculaId) != 0)
+            {
+                pelicula.PeliculaID = int.Parse(PeliculaId);
+            }
+            pelicula.Genero= Genero;
+            pelicula.Nombre = Nombre;
+            pelicula.Existencia = int.Parse(Existencia);
+            pelicula.Autores = Autores;
+            pelicula.PrecioRenta = decimal.Parse(PrecioRenta);
+            pelicula.Estado = ChkActivo.Checked;
+            nPeliculas.GuardarPelicula(pelicula);
+            cargar();
+            Limpiar();
+        }
+        void cargar()
+        {
+            DgvDatos.DataSource = nPeliculas.ObtenerTodasLasPeliculas();
+        }
+
+        private void Peliculas_Load(object sender, EventArgs e)
+        {
+            cargar();
+        }
+        void Editar()
+        {
+            if (DgvDatos.SelectedCells.Count > 0)
+            {
+                int rowIndex = DgvDatos.SelectedCells[0].RowIndex;
+                DataGridViewCheckBoxCell checkBoxCell = DgvDatos.Rows[rowIndex].Cells["Seleccion"] as DataGridViewCheckBoxCell;
+
+                if (checkBoxCell?.Value is true)
+                {
+                    // Obtener el ID directamente de la celda
+                    if (DgvDatos.Rows[rowIndex].Cells["PeliculaID"].Value is int id)
+                    {
+                        ConsultarPorID(id);
+                    }
+                }
+            }
+        }
+        void ConsultarPorID(int IDPelicula)
+        {
+            var pelicula = nPeliculas.ObtenerTodasLasPeliculas().FirstOrDefault(g => g.PeliculaID == IDPelicula);
+            if (pelicula != null)
+            {
+                LblId.Text = pelicula.PeliculaID.ToString();
+                TxtNombre.Text = pelicula.Nombre;
+                TxtAutores.Text = pelicula.Autores;
+                TxtGenero.Text = pelicula.Genero;
+                TxtExistencia.Text = pelicula.Existencia.ToString();
+                TxtPrecioRenta.Text = pelicula.PrecioRenta.ToString();
+                ChkActivo.Checked = pelicula.Estado;
+            }
+        }
+
+        private void BtnEditar_Click(object sender, EventArgs e)
+        {
+            Editar();
+        }
+        private void Eliminar()
+        {
+            if (DgvDatos.SelectedCells.Count > 0)
+            {
+                int rowIndex = DgvDatos.SelectedCells[0].RowIndex;
+                DataGridViewCheckBoxCell checkBoxCell = DgvDatos.Rows[rowIndex].Cells["Seleccion"] as DataGridViewCheckBoxCell;
+
+                if (checkBoxCell?.Value is true)
+                {
+                    // Obtener el ID directamente de la celda
+                    if (DgvDatos.Rows[rowIndex].Cells["PeliculaID"].Value is int id)
+                    {
+                        if (MessageBox.Show("¿Está seguro de eliminar este cliente?", "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            nPeliculas.EliminarPelicula(id);
+                            cargar();
+                            Limpiar();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            Eliminar();
+        }
+    }
+}
