60738df6cdd37aedda1bcdb1acdbb596dfd16297
Jesús Eduardo <9107140aa8@gmail.com>
2024-03-24 18:06:58 -0600

    Adicion Tablas Peliculas/Clientes

---
+++ Capa Datos/Migrations/202403250000063_Tablas.cs
@@ -0,0 +1,44 @@
+﻿namespace Capa_Datos.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Tablas : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clientes",
+                c => new
+                    {
+                        ClienteId = c.Int(nullable: false, identity: true),
+                        Nombres = c.String(nullable: false, maxLength: 75),
+                        Apellidos = c.String(nullable: false, maxLength: 75),
+                        FechaIngreso = c.DateTime(nullable: false),
+                        Estado = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ClienteId);
+            
+            CreateTable(
+                "dbo.Peliculas",
+                c => new
+                    {
+                        PeliculaID = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(nullable: false, maxLength: 75),
+                        Genero = c.String(nullable: false, maxLength: 30),
+                        Autores = c.String(nullable: false, maxLength: 75),
+                        Existencia = c.Int(nullable: false),
+                        PrecioRenta = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Estado = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.PeliculaID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Peliculas");
+            DropTable("dbo.Clientes");
+        }
+    }
+}
